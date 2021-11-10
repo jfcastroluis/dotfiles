f16ef7c2f15c019f9ad3ce6ffb46607572b1d11b
Jose Castro <jfcastroluis@gmail.com>
2021-11-10 15:55:49 +0000

    run: install packages

---
+++ run_packages_install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt install htop \
+                 imagemagick \
+                 jq \
+                 pandoc \
+                 pwgen \
+                 ranger \
+                 texlive \
+                 tldr \
+                 tree \
+                 virtualenvwrapper \
+                 vlc \
+                 whois
